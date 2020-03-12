633d496c80dbdde7d6a41581a2ff8a21a27e9fbb
Italo Russo <italrr@gmail.com>
2020-03-12 18:38:09 -0400

    finished inventory

--- src/Entity/Inventory.cpp
+++ src/Entity/Inventory.cpp
@@ -1,3 +1,65 @@
 #include "Inventory.hpp"
 #include "../Engine/Tools/Tools.hpp"
 
+Game::InventoryStat::InventoryStat(){
+	seedIndex = nite::randomInt(5, 35);
+}
+
+UInt16 Game::InventoryStat::add(Shared<Game::BaseItem> &item){
+	if(item.get() == NULL){
+		return 0;
+	}
+	seedIndex += 1;
+	int finalId = -1;
+	int id = item->id;
+	auto ins = get(item->id); // check if this item is already in the carry
+	if(ins.get() == NULL || !item->amnt){
+		carry[seedIndex] = item;	
+		item->slotId = seedIndex;
+		item->onCarryAdd();
+		finalId = seedIndex;
+	}else{
+		ins->qty += item->qty;
+		finalId = ins->slotId;
+	}
+	return finalId;
+}
+
+Shared<Game::BaseItem> Game::InventoryStat::get(UInt16 itemId){
+	for (auto &pair : carry) {
+		if(pair.second->id == itemId){
+			return carry[itemId];
+		}
+	}	
+	return Shared<Game::BaseItem>(NULL);
+}
+
+bool Game::InventoryStat::contains(UInt16 id){
+    return carry.find(id) != carry.end();
+}
+
+void Game::InventoryStat::clear(){
+    for(auto &it : carry){
+        remove(it.second->id, 0); // 0 = means ALL
+    }
+}
+
+bool Game::InventoryStat::remove(UInt16 id, UInt16 amnt){
+	if(!contains(id)){
+		return false;
+	}
+	for(auto &it : carry){
+        auto &item = *it.second.get();
+        if(item.id == id){
+            if(item.amnt){
+                item.qty -= amnt;
+            }
+            if(amnt == 0 || item.qty < 0){
+                item.onCarryRemove();
+                item.slotId = 0;
+                carry.erase(it.first);                
+            }              
+        }
+    }
+    return true;
+}
--- src/Entity/Inventory.hpp
+++ src/Entity/Inventory.hpp
@@ -7,86 +7,98 @@
 	#include "../Core/Object.hpp"
   	#include <memory>
 
-  namespace Game {
+	namespace Game {
 
-	namespace Element {
-		enum Element : UInt8 {
-			None = 0,
-			Neutral,
-			Fire,
-			Electric,
-			Steel,
-			Wind,
-			Umbra,
-			Celestial,
-			Emerium
-		};
-		static String name(int id){
-			switch(id){
-				case Neutral:
-					return "Neutral";
-				case Fire:
-					return "Fire";
-				case Electric:
-					return "Electric";
-				case Umbra:
-					return "Umbra";	
-				case Celestial:
-					return "Celestial";							
-				case Steel:
-					return "Steel";			
-				case Emerium:
-					return "Emerium";			
-				default:
-					return "None";																										
+		namespace Element {
+			enum Element : UInt8 {
+				None = 0,
+				Neutral,
+				Fire,
+				Electric,
+				Steel,
+				Wind,
+				Umbra,
+				Celestial,
+				Emerium
+			};
+			static String name(int id){
+				switch(id){
+					case Neutral:
+						return "Neutral";
+					case Fire:
+						return "Fire";
+					case Electric:
+						return "Electric";
+					case Umbra:
+						return "Umbra";	
+					case Celestial:
+						return "Celestial";							
+					case Steel:
+						return "Steel";			
+					case Emerium:
+						return "Emerium";			
+					default:
+						return "None";																										
+				}
 			}
 		}
-	}
 
-	namespace ItemType {
-		enum ItemType : UInt8 {
-			Undefined = 0,
-			Equip,
-			Consumable,
-			Etc,
-			Chestwar,
-			Footwear,
-			Headwear,
-			RightAcc,
-			LeftAcc
+		namespace ItemType {
+			enum ItemType : UInt8 {
+				None = 0,
+				Equip, // weapons
+				Armor, // armor, shield, footwear, etc
+				Usable, // potions, scrolls
+				Etc // everything else
+			};
+		}
+
+		struct BaseItem {
+			UInt16 id;
+			UInt16 slotId;
+			UInt8 type;
+			String name;
+			String desc;
+			UInt16 weight;
+			Int32 qty;
+			UInt16 elemnt;
+			bool amnt;
+
+			BaseItem(){
+				slotId = 0; // it can never be 0
+			}
+			
+			virtual void use(){
+
+			}
+			
+			virtual void onAfterUse(){
+
+			}
+			
+			virtual void onBeforeUse(){
+
+			}
+			
+			virtual void onCarryAdd(){
+
+			}
+
+			virtual void onCarryRemove(){
+
+			}
+		};
+
+		struct InventoryStat {
+			UInt16 seedIndex;
+			Dict<UInt16, Shared<Game::BaseItem>> carry;
+			InventoryStat();
+			UInt16 add(Shared<Game::BaseItem> &item);
+			Shared<Game::BaseItem> get(UInt16 itemId);
+			bool contains(UInt16 id);
+			void clear();
+			bool remove(UInt16 id, UInt16 amnt);		
 		};
 	}
-
-	struct BaseItem {
-		UInt16 id;
-		UInt8 type;
-		String name;
-		String description;
-		UInt16 weight;
-		UInt32 quantity;
-		UInt16 element;
-		bool amountable;
-		
-		virtual void use(){
-
-		}
-		
-		virtual void onAfterUse(){
-
-		}
-		
-		virtual void onBeforeUse(){
-
-		}
-		
-		virtual void onCarryAdd(){
-
-		}
-
-		virtual void onCarryRemove(){
-
-		}
-	};
-  }
 
 #endif
