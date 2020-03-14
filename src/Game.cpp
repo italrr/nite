994598ce8f6bb8f3e9a5bdfb0935cab15325d004
Italo Russo <italrr@gmail.com>
2020-03-14 13:09:43 -0400

    split entry points to add Dedicated server bin

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,9 +1,5 @@
 #include "Game.hpp"
 #include "Engine/View.hpp"
-#include "Engine/Graphics.hpp"
-#include "Engine/Input.hpp"
-#include "Engine/nScript.hpp"
-#include "Engine/UI/UI.hpp"
 
 static Game::GameCore *instance = NULL;
 
@@ -42,33 +38,4 @@ void Game::GameCore::render(){
 
 Game::GameCore *Game::getGameCoreInstance(){
 	return instance;
-}
-
-int main(int argc, char* argv[]){
-	Vector<String> params;
-	for(int i = 0; i < argc; ++i){
-		params.push_back(String(argv[i]));
-	}
-	nite::setParameters(params);
-	
-	Game::GameCore game;
-	game.start();
-
-	game.localSv.preinit();
-	game.localSv.setupGame("Pacifier's corner", 4, 1);
-
-	game.client.setup("pepper"+nite::toStr(nite::randomInt(25, 50)));
-
-	nite::AsyncTask::spawn(nite::AsyncTask::ALambda([&](nite::AsyncTask::Context &ct){
-		game.client.connect("127.0.0.1", nite::NetworkDefaultPort);
-		ct.stop();
-	}), 1000);
-
-	while(game.isRunning){			
-		game.update();
-		game.render();
-	}
-	game.onEnd();
-
-	return 0;
 }
